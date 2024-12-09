29abf4347ef7dc866a291918d832fa0494a1a886
wesamabuaqel138 <wesamabuaqel138@gmail.com>
2024-12-10 00:53:17 +0300

    Third

--- Abood/src/app/app.routes.ts
+++ Abood/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import {provideRouter, Routes} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {SignupComponent } from './signup/signup.component';
 import {ApplicationConfig} from '@angular/core';
+import {HomepageComponent} from './homepage/homepage.component';
 
 
 export const routes: Routes = [
+  { path: '', component: HomepageComponent},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
